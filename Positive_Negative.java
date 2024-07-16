b2d9db856c96bcd6cf091bb0966504cd1d45b31a
aswajbs <aswajbs@gmail.com>
2024-07-16 10:44:47 +0530

    Positive_Negative Program

---
+++ Positive_Negative.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Positive_Negative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        if (num < 0)
+            System.out.println("Negative number");
+        else
+            System.out.println("Positive number");
+    }
+}
